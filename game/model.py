512afbbbddea415a11a4eadfb5acc6922e9ee0ac
浜田　俊祐 <hamashun0820@icloud.com>
2023-10-04 18:33:30 +0900

    position周りのバグを修正

--- game/model.py
+++ game/model.py
@@ -15,18 +15,37 @@ class Model:
         self.score = Score()
         self.food = Food(self.get_empty_position())
         self.game_over = False
+        self._board = [[0 for _ in range(Stage.WIDTH)] for _ in range(Stage.HEIGHT)]
 
     def update(self):
-        if Judger.check_back(self.snake):
-            self.snake.move_according_to_inertia()
+        try:
+            if Judger.check_back(self.snake):
+                self.snake.move_according_to_inertia()
+                self.game_over = True
+                return
+        except IndexError:
+            self.game_over = True
             return
-        self.snake.move()
+        
+        try:
+            self.snake.move()
+        except ValueError:
+            self.game_over = True
+            return
         if Judger.check_collision_food(self.snake, self.food):
             self.snake.grow()
             self.score.add()
             self.food = Food(self.get_empty_position())
         if Judger.check_collision_self(self.snake) or Judger.check_collision_wall(self.snake):
             self.game_over = True
+
+        if not self.game_over:
+            self._board = [[0 for _ in range(Stage.WIDTH)] for _ in range(Stage.HEIGHT)]
+            self._board[self.snake.head.y - 1][self.snake.head.x - 1] = 1
+            for body in self.snake.body:
+                self._board[body.y - 1][body.x - 1] = 2
+
+            self._board[self.food.pos.y - 1][self.food.pos.x - 1] = 3
 
     def get_empty_position(self) -> Position:
         while True:
@@ -39,11 +58,4 @@ class Model:
 
     @property
     def board(self) -> list[list[int]]:
-        board = [[0 for _ in range(Stage.WIDTH)] for _ in range(Stage.HEIGHT)]
-        board[self.snake.head.y - 1][self.snake.head.x - 1] = 1
-        for body in self.snake.body:
-            board[body.y - 1][body.x - 1] = 2
-
-        board[self.food.pos.y - 1][self.food.pos.x - 1] = 3
-
-        return board
+        return self._board
--- game/position.py
+++ game/position.py
@@ -1,7 +1,12 @@
+from .stage import Stage
+
+
 class Position:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+        if x < 0 or x > Stage.WIDTH + 1 or y < 0 or y > Stage.HEIGHT + 1:
+            raise ValueError("Invalid position")
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
