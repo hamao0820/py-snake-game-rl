8f46683aa0e1bb557b408ac0874af07a868d8b9f
浜田　俊祐 <hamashun0820@icloud.com>
2023-10-04 10:22:24 +0900

    judgerをすっきりさせた

--- game/judger.py
+++ game/judger.py
@@ -5,24 +5,17 @@ from stage import Stage
 
 class Judger:
     @staticmethod
-    def check_collision_self(self, snake: Snake) -> bool:
-        if snake.head in snake.body:
-            return True
-        return False
+    def check_collision_self(snake: Snake) -> bool:
+        return snake.head in snake.body
 
     @staticmethod
-    def check_collision_wall(self, snake: Snake) -> bool:
-        if (
-            snake.position_list[0].x < 1
-            or snake.position_list[0].x > Stage.WIDTH
-            or snake.position_list[0].y < 1
-            or snake.position_list[0].y > Stage.HEIGHT
-        ):
-            return True
-        return False
+    def check_collision_wall(snake: Snake) -> bool:
+        return snake.head.x < 1 or snake.head.x > Stage.WIDTH or snake.head.y < 1 or snake.head.y > Stage.HEIGHT
 
     @staticmethod
-    def check_collision_food(self, snake: Snake, food: Food) -> bool:
-        if snake.head == food.pos:
-            return True
-        return False
+    def check_collision_food(snake: Snake, food: Food) -> bool:
+        return snake.head == food.pos
+
+    @staticmethod
+    def check_back(snake: Snake) -> bool:
+        return snake.next_position == snake.body[0]
